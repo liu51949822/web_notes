3546ec84b33c855334b714fee1a579da117260ca
liurenjie <1240332437@qq.com>
2025-09-25 18:13:58 +0800

    降级

--- web_notes/next.config.js
+++ web_notes/next.config.js
@@ -8,27 +8,20 @@ const nextConfig = {
     unoptimized: true,
   },
   
-  // Turbopack配置
-  experimental: {
-    turbo: {
-      // 启用更快的开发构建
-      enabled: true,
-      // 避免在开发模式下进行类型检查
-      skipTypeChecks: true,
-      // 优化客户端捆绑
-      optimizeClientBundles: true,
-    },
-  },
-  
   // 生产环境优化
   productionBrowserSourceMaps: false,
-  
+
   // 构建时优化
   optimizeFonts: true,
   
-  // 解决之前出现的Turbopack根目录警告
-  turbopack: {
-    root: process.cwd(),
+  experimental: {
+    outputFileTracingIncludes: {
+      '/': [
+        './node_modules/styled-jsx/**/*',
+        // 如果还有其他 transpile 的包，也加进来
+        // './node_modules/some-other-package/**/*'
+      ],
+  },
   },
 };
 
